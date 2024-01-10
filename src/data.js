dfdedeb65b61034f584402459a0597d05189364d
Mohit <mohitverma8817@gmail.com>
2024-01-10 16:47:28 +0530

    about sec add

--- src/data.js
+++ src/data.js
@@ -29,10 +29,10 @@ export const links = [
         name: "Home",
         path: '/'
     },
-    //  {
-    //     name: "About",
-    //     path: '/about'
-    // },
+     {
+        name: "About",
+        path: '/about'
+    },
     {
         name: "Mens",
         path: '/mens'
